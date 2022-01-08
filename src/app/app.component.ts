a5023634f55674e35d304d30f7a053c4117c1bbd
Ebrahim Mozaffari <ebrahim.mozafary@gmail.com>
2022-01-08 21:23:16 +0330

    life cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   //@ViewChild('par',{static:true,read:ElementRef}) par:ElementRef ;
   @ViewChild(IncComponent, {static: true, read: IncComponent}) incComp: IncComponent | undefined;
   @ViewChildren(IncComponent, {read: IncComponent}) inComp1: QueryList<IncComponent> | undefined;
-  // @ViewChild('par') par: ElementRef | undefined ;
+   @ViewChild('par') par: ElementRef | undefined ;
 
   constructor(private renderer: Renderer2) {
 
@@ -67,9 +67,9 @@ export class AppComponent implements OnInit, AfterViewInit {
     //this.par.nativeElement.innerHTML ='123456';
   }
 
-//   public onContentChange(){
-// // @ts-ignore
-//     //yademon bashe in kar ro hichvaght anjam nadim faghat ba renderer2 taghir bedim
-//     this.par?.nativeElement.textContent = '123456'
-//   }
+  public onContentChange(){
+// @ts-ignore
+    //yademon bashe in kar ro hichvaght anjam nadim faghat ba renderer2 taghir bedim
+    this.par?.nativeElement.textContent = '123456'
+  }
 }
--- src/app/lifecycle/lifecycle.component.ts
+++ src/app/lifecycle/lifecycle.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked,
+  AfterViewInit,
+  Component, ContentChild,
+  DoCheck, ElementRef, Input,
+  OnChanges, OnDestroy,
+  OnInit, SimpleChanges, ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle',
   templateUrl: './lifecycle.component.html',
   styleUrls: ['./lifecycle.component.sass']
 })
-export class LifecycleComponent implements OnInit {
+export class LifecycleComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,OnDestroy
+{
+  @ViewChild('par2',{static:true}) par2 :ElementRef | undefined;
+  @ContentChild("par",{static:true}) par : ElementRef | undefined;
+  @Input() var1:any;
 
   constructor() { }
-
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges):void {
+    throw new Error('method not implement')
   }
 
+  ngOnInit(): void {
+    console.log('ngOnInit',this.par?.nativeElement.textContent)
+  }
+
+  ngDoCheck():void {
+    console.log('ngDoCheck was caled')
+  }
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit',this.par?.nativeElement.textContent)
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked',this.par?.nativeElement.textContent)
+  }
+  ngAfterViewInit():void {
+    console.log('ngAfterViewInit',this.par2?.nativeElement.textContent)
+  }
+  ngAfterViewChecked():void {
+    console.log('ngAfterViewChecked',this.par2?.nativeElement.textContent)
+  }
+  ngOnDestroy():void {
+    console.log("component destroyed")
+  }
+
+  public onChangedViewChild(){
+    // @ts-ignore
+    this.par2?.nativeElement.textContent = 'viewChildChanged'
+  }
 }
